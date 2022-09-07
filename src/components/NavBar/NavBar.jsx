7f42431aad171f31bdca8edfcd82fb7e53fe8f01
GUchechi <godswilluchendu7@gmail.com>
2022-09-07 12:52:00 +0100

    animated effect

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Typewriter } from 'typewriter-effect'
 import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
+    <div className='navbar'>
+        <h2 className='name'>Godswill Uchechi</h2>
+    </div>
+
   )
 }
 
