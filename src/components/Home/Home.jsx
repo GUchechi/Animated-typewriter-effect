6c3eb0d51903ca5ea54e35bad34562d0124ac943
GUchechi <godswilluchendu7@gmail.com>
2022-09-07 10:52:06 +0100

    added typewriter animation  effect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Typewriter from 'typewriter-effect';
 import './Home.css'
 
 const Home = () => {
+    const [state] = useState({
+        title: 'Hi,',
+        titleTwo: "I'm",
+        titleThree: "Godswill Uchechi",
+        image: ""
+    })
+
+
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='home-intro'>
+            <h2>
+                <div className='title'>{state.title}</div>
+                <div className='titleTwo'>{state.titleTwo}</div>
+                <div className='titleThree'>{state.titleThree}</div>
+            </h2>
+            <div className='text'>
+                <Typewriter 
+                    options={{
+                        autoStart: true,
+                        loop: true,
+                        delay:40,
+                        strings: [
+                            "I'm a Frontend Developer",
+                            "I love React Js",
+                            "I'm a Tech Enthusiast"
+                        ],
+                    }}/>
+            </div>
+        </div>
+    </div>
   )
 }
 
